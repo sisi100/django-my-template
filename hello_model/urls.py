0e7eeed3c860bd13ade4d815b319b39f503b1bc0
Shun Isii <sisii00.swork@gmail.com>
2020-09-09 22:56:50 +0900

    更新、削除のAPI追加

--- hello_model/urls.py
+++ hello_model/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
 
-from .views import user_list, user_create
+from .views import user_create, user_delete, user_list, user_update
 
 urlpatterns = [
     path("users/", user_list),
-    path("users/<int:user_id>", user_create),
+    path("users/<int:user_id>/create/", user_create),
+    path("users/<int:user_id>/update/", user_update),
+    path("users/<int:user_id>/delete/", user_delete),
 ]
--- hello_model/views.py
+++ hello_model/views.py
@@ -7,7 +7,9 @@ from .models import User
 def user_list(request: WSGIRequest) -> JsonResponse:
     """ ユーザー一覧を表示する """
     users = User.objects.all()
-    user_list = [{"id": user.id, "name": user.name} for user in users]
+    user_list = [
+        {"id": user.id, "name": user.name, "age": user.age} for user in users
+    ]
 
     return JsonResponse({"user_list": user_list})
 
@@ -15,7 +17,28 @@ def user_list(request: WSGIRequest) -> JsonResponse:
 def user_create(request: WSGIRequest, user_id: int) -> JsonResponse:
     """ 新規にユーザーを作る """
     name = request.GET.get("name") or "No Name"
-    user = User(id=user_id, name=name)
+    age = request.GET.get("age") or "20"
+    user = User(id=user_id, name=name, age=age)
     user.save()
 
-    return JsonResponse({"id": user.id, "name": user.name})
+    return JsonResponse({"id": user.id, "name": user.name, "age": user.age})
+
+
+def user_update(request: WSGIRequest, user_id: int) -> JsonResponse:
+    """ ユーザー情報を更新する """
+    user = User.objects.get(id=user_id)
+    if name := request.GET.get("name"):
+        user.name = name
+    if age := request.GET.get("age"):
+        user.age = age
+    user.full_clean()
+    user.save()
+
+    return JsonResponse({})
+
+
+def user_delete(request: WSGIRequest, user_id: int) -> JsonResponse:
+    """ ユーザーを削除する """
+    user = User.objects.get(id=user_id)
+    user.delete()
+    return JsonResponse({})
